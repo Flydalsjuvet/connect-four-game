e88329122be47e42bf512e01b6c33acce05fb71b
Adam Wolff <mikelmagallanes@gmail.com>
2020-05-14 17:29:08 -0400

    Completed changes up to step 4

--- connect4.js
+++ connect4.js
@@ -5,49 +5,54 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
-function makeBoard() {
+const makeBoard = () => {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
 
-for (let i = 0; i < HEIGHT; i++){
-  board[i] = [];
-  for (let j = 0; j < WIDTH; j++){
-    board[i][j] = null;
+  for (let i = 0; i < HEIGHT; i++){
+    board[i] = [];
+      for (let j = 0; j < WIDTH; j++){
+        board[i][j] = null;
+      }
   }
-}
 
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
-function makeHtmlBoard() {
+const makeHtmlBoard = () => {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  let htmlBoard = document.getElementById("board");
 
-  // TODO: add comment for this code
-  var top = document.createElement("tr");
+  // Creates table row with id of column-top, and runs handleClick function when table row gets clicked
+  // column-top is filled with table data cells with numbered id 0 to WIDTH and they are appended to table row column-top
+  // table row appended to htmlBoard
+
+  const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  for (let x = 0; x < WIDTH; x++) {
+    const headCell = document.createElement("td");
     headCell.setAttribute("id", x);
     top.append(headCell);
   }
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
+  // Creating table row elements of play, and inserting with table data elements of id denoting row and position
+  //cells are appended to respective table rows and these are sequentially appended to DOM board element
+  for (let y = 0; y < HEIGHT; y++) {
     const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
